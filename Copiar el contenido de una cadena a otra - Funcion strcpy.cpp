4834063355ae94b9500093894270f61b152f6f70
fnoel <noelonassis@gmail.com>
2019-04-21 14:38:37 -0600

    nuevos algoritmos de longitud de caracteres

---
+++ Copiar el contenido de una cadena a otra - Funcion strcpy.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+using namespace std;
+
+int main() {
+char nombre[] = "Alejandro";
+char nombre2[20];
+
+strcpy(nombre2, nombre);
+
+cout<<nombre2<<endl;
+
+
+
+getch();
+return 0;
+}
---
+++ logirud de palabras.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+using namespace std;
+
+int main (){
+	char palabra[]= "hola que tal";
+	int longitud=0;
+	
+	longitud = strlen(palabra);
+	
+	cout<<"Numero de elementos de la cadana es: "<<longitud<<endl;
+	
+	
+	
+	getch();
+	return 0;
+}
