51dcc9b58e91eec80c50cb2255cbed4853fd5bec
fnoel <noelonassis@gmail.com>
2019-08-10 11:31:29 -0600

    Ejercio de comparar cadenas

---
+++ comparar cadenas.cpp
@@ -0,0 +1,24 @@
+//camparar cadena - función strcmp()
+
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+ 
+using namespace std;
+
+int main(){
+	
+	char palabra1[]="Hola";
+	char palabra2[]="Hola";
+	
+	if(strcmp(palabra1,palabra2)==0){
+	 cout<<"Ambas cadenas son iguales";
+
+	}
+	getch();
+	return 0;
+}
+
+
+	
+
