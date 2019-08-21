49019a21abf81a257f8679609f18a62b591265c6
fnoel <noelonassis@gmail.com>
2019-08-20 21:49:44 -0600

    función strev

---
+++ funcion strrev.cpp
@@ -0,0 +1,20 @@
+//invertir una cadena - funcion strrev()
+
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+	
+	char cad[] = "Anita";
+	strrev(cad);
+	
+	cout<<cad<<endl;
+	
+	
+	
+	getch();
+	return 0;
+}
