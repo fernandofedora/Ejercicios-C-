af76643801868b4e8ee79147c0a8e23b9b10c60f
fnoel <noelonassis@gmail.com>
2019-12-27 19:42:09 -0600

    funcion strlwr
    Programación en C++ || Cadenas || Pasar una palabra a minúscula - Función strlwr()

---
+++ fucion strllwr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <conio.h>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+char palabra[]="PRARAMACION";
+
+strlwr(palabra);
+
+cout<<palabra<<endl;
+
+getch();
+return 0;
+}
